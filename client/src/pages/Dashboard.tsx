c9f167902508e764345fe7c5dc1d5d6c5999d076
KaeTheDev <kaethedev@gmail.com>
2025-12-30 00:08:56 -0500

    Added log out button to Dashboard and UserProfile

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
 export const Dashboard = () => {
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        window.location.href = '/';
+    };
+
     return(
         <>
         <h1 className="text-3xl text-pink-700">Welcome to the Dashboard!</h1>
+        <button onClick={handleLogout} className="fixed top-3 right-20 p-3 bg-red-500 text-white rounded-full shadow-lg hover:bg-red-600">Log Out</button>
         </>
     )
 }
