a32fdb007001be21e4ebf773c1113d54a6560342
KaeTheDev <kaethedev@gmail.com>
2026-01-04 19:49:45 -0500

    Created Sidebar component

---
+++ client/src/components/UserProfile/Sidebar/Sidebar.tsx
@@ -0,0 +1,45 @@
+interface SidebarProps {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
+}
+
+const Sidebar = ({
+  activeTab,
+  setActiveTab,
+  isOpen,
+  setIsOpen,
+}: SidebarProps) => {
+  return (
+    <div
+      className={`w-64 shrink-0 bg-white shadow-lg border flex flex-col h-screen z-40 
+            lg:static fixed inset-y-0 left-0 transform transition-transform duration-300 ${
+              isOpen ? "translate-x-0" : "-translate-x-full"
+            } lg:translate-x-0`}
+    >
+      <div className="flex-1 p-6 mt-7 space-y-2 overflow-auto">
+        {["profile", "my orders", "my reviews", "account settings"].map(
+          (tab) => (
+            <button
+              key={tab}
+              onClick={() => {
+                setActiveTab(tab);
+                setIsOpen(false);
+              }}
+              className={`w-full text-left py-3 px-4 rounded-xl transition-all ${
+                activeTab === tab
+                  ? "bg-blue-200 text-blue-800 shadow-lg"
+                  : "text-gray-700 hover:bg-gray-50"
+              }`}
+            >
+              {tab.charAt(0).toUpperCase() + tab.slice(1)}
+            </button>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
