c34fa9e35f3e2458dceeea1bc4656207c3fe25b3
KaeTheDev <kaethedev@gmail.com>
2026-01-06 23:55:01 -0500

    Built AccountSettings layout with tailwindcss and dummy data

--- client/src/components/UserProfile/AccountSettings/AccountSettings.tsx
+++ client/src/components/UserProfile/AccountSettings/AccountSettings.tsx
@@ -1,6 +1,49 @@
 const AccountSettings = () => {
-    return (
-        <p className="text-3xl text-red-500">Account Settings Tab</p>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-col bg-white p-4 rounded-2xl shadow w-full gap-4">
+        <div className="flex flex-row justify-between">
+          <p className="text-sm text-gray-500 mb-4">Profile Information</p>
+          <p className="text-sm text-gray-500">Edit</p>
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col mr-1">ICON</div>
+          <div className="flex flex-col">
+            <p className="text-sm text-gray-500">Full Name</p>
+            <p className="text-sm text-gray-500">Sophia Laurent</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row">
+          <div className="flex flex-col mr-1">ICON</div>
+          <div className="flex flex-col">
+            <p className="text-sm text-gray-500">Email Address</p>
+            <p className="text-sm text-gray-500">sophia.laurent@example.com</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col bg-white p-4 rounded-2xl shadow w-full gap-4 mt-4">
+        <div className="flex flex-row justify-between">
+          <p className="text-sm text-gray-500">Password</p>
+          <p className="text-sm text-gray-500">Change</p>
+        </div>
+        <p className="text-sm text-gray-500">
+          Update your password to keep your account secure
+        </p>
+        <span className="text-gray-500">******</span>
+      </div>
+
+      <div className="flex flex-col bg-white p-4 rounded-2xl shadow w-full gap-4 mt-4 border-1 border-red-600">
+        <p className="text-sm text-gray-500">Danger Zone</p>
+        <p className="text-sm text-gray-500">
+          Permanently delete your account and all associated data
+        </p>
+        <button className="self-start bg-red-100 text-red-600 text-sm px-3 py-1.5 rounded-lg hover:bg-red-200 transition">
+          Delete Account
+        </button>
+      </div>
+    </>
+  );
+};
 export default AccountSettings;
