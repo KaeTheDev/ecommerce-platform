f877aab74f1a232cfde0f78adfbc016943fcf960
KaeTheDev <kaethedev@gmail.com>
2026-01-10 01:07:47 -0500

    Added update route to users.route

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import express, { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { getCurrentUser } from "../controllers/userController";
+import { getCurrentUser, updateCurrentUser } from "../controllers/userController";
 
 const router: Router = express.Router();
 
-// Protected route = only logged-in users
+// Protected route = only logged-in users: Get User
 router.get("/me", authMiddleware, getCurrentUser);
+
+// Protected route = only logged-in users: Update User
+router.patch("/me", authMiddleware, updateCurrentUser);
 
 export default router;
