838772aadd9adb572f16638cc7133d0e053958ad
KaeTheDev <kaethedev@gmail.com>
2026-01-15 22:26:34 -0500

    Removed CartModal from home page. testing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,7 @@
-import CartModal from "../components/Cart/CartModal/CartModal";
-
 export const Home = () => {
   return (
     <>
       <h1 className="text-3xl text-purple-700">Welcome to Luxarist!</h1>
-      <CartModal />
     </>
   );
 };
