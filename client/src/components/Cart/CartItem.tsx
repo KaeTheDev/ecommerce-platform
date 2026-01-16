9948224794579d03f3efdf049d9ee538db57dbb2
KaeTheDev <kaethedev@gmail.com>
2026-01-15 19:32:01 -0500

    created cart item component

---
+++ client/src/components/Cart/CartItem.tsx
@@ -0,0 +1,22 @@
+import QuantitySelector from "../QuantitySelector/QuantitySelector"
+
+const CartItem = () => {
+  return (
+    <div className="flex items-center gap-4 py-2">
+      <img
+        src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1200&q=80"
+        alt="test product"
+        className="w-20 h-20 object-cover rounded"
+      />
+
+      <div className="flex flex-col text-sm flex-1">
+        <p className="font-medium">Product Name</p>
+        <p className="text-gray-500">$49.99</p>
+      </div>
+
+      <QuantitySelector />
+    </div>
+  )
+}
+
+export default CartItem
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,21 +1,10 @@
-import ProductDetailsTabs from "../components/ProductCard/ProductDetailsTab/ProductDetailsTab";
-import ProductImageGallery from "../components/ProductCard/ProductImageGallery/ProductImageGallery";
-import ProductInformation from "../components/ProductCard/ProductInformation/ProductInformation";
-import ProductRecommendations from "../components/ProductRecommendations/ProductRecommendations";
+import CartItem from "../components/Cart/CartItem";
 
 export const Home = () => {
   return (
     <>
       <h1 className="text-3xl text-purple-700">Welcome to Luxarist!</h1>
-      <div className="flex flex-col">
-        <div className="flex flex-row">
-          <ProductImageGallery />
-          <ProductInformation />
-        </div>
-        <ProductDetailsTabs />
-      </div>
-
-      <ProductRecommendations />
+      <CartItem />
     </>
   );
 };
